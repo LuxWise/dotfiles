31287de09da45ed895635b3dda6a4168e09b049c
Camilo Sanchez <111465555+LuxWise@users.noreply.github.com>
2023-07-21 00:28:48 +0000

    Create launch.sh

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1 and bar2
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar example -c ~/.config/polybar/config.ini &
